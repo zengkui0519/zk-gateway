a44f065bedc00ecebc7ddcad7960c98d8641ceee
zengkui <>
2021-04-10 18:05:30 +0800

    “网关服务”

---
+++ src/main/java/com/zk/gateway/GatewayApplication.java
@@ -0,0 +1,18 @@
+package com.zk.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@EnableDiscoveryClient
+@ComponentScan
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+}
